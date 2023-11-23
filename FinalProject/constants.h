14006d40d972e73e4367a3016d945c15679b6221
willsonr8 <willsonr8@gmail.com>
2023-11-22 23:32:50 -0500

    Allow main to build blank window based on constant values.

---
+++ FinalProject/constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int WINDOW_WIDTH = 1200;
+const int WINDOW_HEIGHT = 800;
+
+#endif // CONSTANTS_H
--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include "constants.h"
+using namespace std; 
+
+sf::RenderWindow* BuildWindow() {
+
+    sf::RenderWindow* windowPtr = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Sorter 1000");
+    return windowPtr;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    sf::RenderWindow* windowPtr = BuildWindow(); 
+    sf::RenderWindow& window = *windowPtr; 
+
+    while (window.isOpen()) {
+
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // allow window closure 
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+    }
 }
 
