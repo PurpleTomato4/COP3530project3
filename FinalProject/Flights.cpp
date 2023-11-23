ecf35719f3d4a85a07eaf4becfe1212ecc5adbef
Ben <92957639+buppgard@users.noreply.github.com>
2023-11-23 14:49:38 -0600

    Add files via upload
    Added Flights class to correct folder.

---
+++ FinalProject/Flights.cpp
@@ -0,0 +1,12 @@
+
+#include "Flights.h"
+
+
+Flights::Flights(float _seats, float _passengers, float _efficiency, string _name, float _distance)
+{
+    seats = _seats;
+    passengers = _passengers;
+    efficiency = _efficiency;
+    name = _name;
+    distance = _distance;
+}
---
+++ FinalProject/Flights.h
@@ -0,0 +1,22 @@
+
+#ifndef PROJECT3_FLIGHTS_H
+#define PROJECT3_FLIGHTS_H
+#include <string>
+using namespace std;
+
+class Flights {
+
+public:
+
+    string name;
+    float seats = 0;
+    float passengers = 0;
+    float distance = 0;
+    float efficiency = 0;
+
+    Flights(float seats, float passengers, float _efficiency ,string _name, float _distance);
+
+};
+
+
+#endif //PROJECT3_FLIGHTS_H
