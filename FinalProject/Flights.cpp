d16c9659c366bc3190356c7f4f3d88254fa7fdc0
JackGoldstein <101548615+JackGoldstein@users.noreply.github.com>
2023-11-24 04:40:54 -0500

    Divide seats by passengers instead

--- FinalProject/Flights.cpp
+++ FinalProject/Flights.cpp
@@ -6,11 +6,11 @@ Flights::Flights(float _seats, float _passengers, string _name, float _distance)
 {
     seats = _seats;
     passengers = _passengers;
-    efficiency = _passengers / _seats;
+    efficiency = _seats / _passengers;
     name = _name;
     distance = _distance;
 
-    if (_seats == 0)
+    if (_passengers == 0)
     {
         efficiency = 0;
     }
