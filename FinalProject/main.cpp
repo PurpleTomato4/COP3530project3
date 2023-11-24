23330c6c2ebc520f78fb335c1832e17ffab8b36f
willsonr8 <willsonr8@gmail.com>
2023-11-23 23:53:55 -0500

    Added menu buttons.

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -3,8 +3,9 @@
 #include <string>
 #include <sstream>
 #include <vector>
-#include "Flights.h"
 #include <SFML/Graphics.hpp>
+#include "Flights.h"
+#include "TextureManager.h"
 #include "constants.h"
 using namespace std; 
 
@@ -63,11 +64,27 @@ int main()
     sf::RenderWindow* windowPtr = BuildWindow(); 
     sf::RenderWindow& window = *windowPtr; 
 
+    sf::Sprite flightSortSprite;
+    sf::Sprite rawDataSprite;
+
+    flightSortSprite.setTexture(TextureManager::GetTexture("AirlineSort"));
+    flightSortSprite.setPosition((WINDOW_WIDTH / 2) - 300, WINDOW_HEIGHT / 2);
+    rawDataSprite.setTexture(TextureManager::GetTexture("SourceData"));
+    rawDataSprite.setPosition(WINDOW_WIDTH / 2 + 100, WINDOW_HEIGHT / 2);
+
     while (window.isOpen()) {
 
         sf::Event event;
         while (window.pollEvent(event))
         {
+            window.clear(); // Clear the window
+
+            // Draw sprites
+            window.draw(flightSortSprite);
+            window.draw(rawDataSprite);
+
+            window.display(); 
+
             // allow window closure 
             if (event.type == sf::Event::Closed)
             {
@@ -75,8 +92,8 @@ int main()
             }
             if (event.type == sf::Event::MouseButtonPressed) {
                 auto mousePosition = sf::Mouse::getPosition(window);
-                int x = (mousePosition.x) / 32;
-                int y = (mousePosition.y) / 32;
+                //int x = (mousePosition.x) / 32;
+                //int y = (mousePosition.y) / 32;
             }
         }
     }
