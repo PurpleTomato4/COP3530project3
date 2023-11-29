905742524eee1eb1712917f6a0c7e8294c194f63
Ben <92957639+buppgard@users.noreply.github.com>
2023-11-29 12:17:55 -0600

    Menu logic improved. Data validation added.

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -12,7 +12,9 @@
 #include "constants.h"
 #include "FlightData.h"
 using namespace std;
-using namespace std::chrono; 
+using namespace std::chrono;
+
+bool validateSelection(string userInput, int lowRange, int highRange);
 
 int main()
 {
@@ -23,42 +25,31 @@ int main()
 
     cout << "Welcome to the Airline Sorter 1000. What would you like to do?" << endl << endl;
 
-    bool menu = true;
-    int counter = 0;
-
-    while (menu) {
-        if (counter > 0) {
-            cout << "Would you like to do anything else?" << endl << endl;
-        }
-        cout << "1. Sort using MergeSort" << endl;
-        cout << "2. Sort using ShellSort" << endl;
-        cout << "3. Sort using QuickSort" << endl;
-        cout << "3. Compare All Sorting Algorithms" << endl;
-        cout << "0. Exit" << endl << endl;
-        counter += 1;
-
-        int sortOption;
-        cin >> sortOption;
-        cout << endl;
-
-        float shellSortGap;
-
-        if (sortOption == 2) {
-            cout << "ShellSort Gap: ";
-            cin >> shellSortGap;
-            cout << endl;
-        }
-
-        cout << "What would you like to sort?" << endl << endl;
-        cout << "1. Air Carriers by Efficiency (Passengers/Available Seats)"
-             << endl;
-        cout << "2. Routes by Efficiency (Passengers/Available Seats)" << endl;
-        cout << "3. All Flights by Distance" << endl;
-        cout << "0. Exit" << endl << endl;
-
+    while (true) {
         int analysisType;
-        cin >> analysisType;
-        cout << endl;
+        bool subMenu = true;
+        while(true) {
+            cout << "What would you like to sort?" << endl << endl;
+            cout << "1. Air Carriers by Efficiency (Passengers/Available Seats)"
+                 << endl;
+            cout << "2. Routes by Efficiency (Passengers/Available Seats)"
+                 << endl;
+            cout << "3. All Flights by Distance" << endl;
+            cout << "0. Exit" << endl << endl;
+
+            string input;
+            cin >> input;
+            cout << endl;
+            if (!validateSelection(input, 0, 3))
+                cout << "Invalid selection. Please try again." << endl << endl;
+            else {
+                analysisType = stoi(input);
+                break;
+            }
+        }
+
+        if (analysisType == 0)
+            break;
 
         Sort<Flights *> sorter;
 
@@ -74,18 +65,66 @@ int main()
             sorter.SetSortFunction(Flights::DistanceComp);
         }
 
-        cout << "In what order would you like to sort?" << endl << endl;
-        cout << "1. Descending" << endl;
-        cout << "2. Ascending" << endl;
+        int sortOption;
+        while(true) {
+            cout << "How would you like to sort the data?" << endl << endl;
+            cout << "1. Sort using MergeSort" << endl;
+            cout << "2. Sort using ShellSort" << endl;
+            cout << "3. Sort using QuickSort" << endl;
+            cout << "4. Compare All Sorting Algorithms" << endl;
+            cout << "0. Exit" << endl << endl;
+
+            string input;
+            cin >> input;
+            cout << endl;
+            if (!validateSelection(input, 0, 4))
+                cout << "Invalid selection. Please try again." << endl << endl;
+            else {
+                sortOption = stoi(input);
+                break;
+            }
+        }
+
+        if (sortOption == 0)
+            break;
+
+        float shellSortGap;
+
+        if (sortOption == 2) {
+            while (true) {
+                cout << "ShellSort Gap: ";
+                string input;
+                cin >> input;
+                cout << endl;
+                if (!validateSelection(input, 0, 100))
+                    cout << "Invalid selection. Please try again." << endl << endl;
+                else {
+                    shellSortGap = stof(input);
+                    break;
+                }
+            }
+        }
 
         int direction;
-        cin >> direction;
-        cout << endl;
+        while (true) {
+            cout << "In what order would you like to sort?" << endl << endl;
+            cout << "1. Descending" << endl;
+            cout << "2. Ascending" << endl;
+            string input;
+            cin >> input;
+            cout << endl;
+            if (!validateSelection(input, 1, 2))
+                cout << "Invalid selection. Please try again." << endl << endl;
+            else {
+                direction = stoi(input);
+                break;
+            }
+        }
 
         bool ascending;
         if (direction == 1)
             ascending = 0;
-        else if (direction == 2)
+        else
             ascending = 1;
 
         sorter.SetSortDirection(ascending);
@@ -105,5 +144,13 @@ int main()
             data.PrintDistanceTop20();
     }
     return 0; 
+}
+
+bool validateSelection(string userInput, int lowRange, int highRange) {
+    if(userInput.size() > 2 || !isdigit(userInput[0]))
+        return false;
+    if (stoi(userInput) < lowRange || stoi(userInput) > highRange)
+        return false;
+    return true;
 }
 
